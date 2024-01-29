5179110098abd86cfd57fb841a317945bae4c0be
mike@mpotra.com <mike@mpotra.com>
2024-01-29 09:21:04 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,24 @@
+# XHTTP
+
+`XHTTP` is a library that defines the [Response](`XHTTP.Response`) and
+[ResponseDocument](`XHTTP.ResponseDocument`) protocols.
+
+For an implementation on top of [Req](https://hex.pm/packages/req)
+see [xhttp_req](https://hex.pm/packages/xhttp_req)
+
+Documentation can be found on [HexDocs](https://hexdocs.pm/xhttp)
+
+## Installation
+
+The package can be installed via [Hex](https://hex.pm)
+by adding `xhttp` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:xhttp, "~> 0.1.0"}
+  ]
+end
+```
+
+License: MIT
---
+++ mix.exs
@@ -0,0 +1,63 @@
+defmodule XHTTP.MixProject do
+  use Mix.Project
+
+  @source_url "https://github.com/mpotra/xhttp"
+  @version "0.1.0"
+
+  def project do
+    [
+      app: :xhttp,
+      version: @version,
+      elixir: "~> 1.14",
+      deps: deps(),
+      consolidate_protocols: Mix.env() != :test,
+      elixirc_paths: elixirc_paths(Mix.env()),
+
+      # Hex
+      description: "",
+      package: package(),
+
+      # Docs
+      name: "Montblanc",
+      docs: docs()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:http_date, "~> 0.1", optional: true},
+      {:timex, "~> 3.7", optional: true},
+      # Docs deps only
+      {:ex_doc, ">= 0.0.0", only: :docs, runtime: false}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: ["Mihai Potra"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url},
+      files: ~w(.formatter.exs mix.exs README.md lib)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "XHTTP",
+      source_ref: "v#{@version}",
+      extra_section: "GUIDES",
+      source_url: @source_url
+    ]
+  end
+end
